2c81763ede5a5acc0fe8ab6f3aeba3396392eec8
Seokxkyu <kyuseok000@gmail.com>
2024-08-26 15:18:30 +0900

    15:18 update

--- py/chat_analyzer.py
+++ py/chat_analyzer.py
@@ -4,6 +4,7 @@ import sys
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode, split, col, regexp_replace, hour
 import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
 if len(sys.argv) != 2:
     print("Usage: chat_analyzer.py <choice>")
@@ -27,7 +28,7 @@ elif choice == 'word':
     df_words = df_words.filter(col("word") != "")
     word_count = df_words.groupBy("word").count().orderBy(col("count").desc()).limit(20)
     word_count_pd = word_count.toPandas() 
-
+    """
     plt.figure(figsize=(10, 6))
     plt.bar(word_count_pd['word'], word_count_pd['count'], color='lightgreen')
     plt.xlabel('Word')
@@ -35,7 +36,17 @@ elif choice == 'word':
     plt.title('Word Frequency')
     plt.xticks(rotation=45)
     plt.show()
+    """
+    # WordCloud 시각화
+    word_freq = dict(zip(word_count_pd['word'], word_count_pd['count']))
+    wordcloud = WordCloud(width=800, height=400, background_color="white").generate_from_frequencies(word_freq)
 
+    plt.figure(figsize=(10, 6))
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
+    plt.title('Word Frequency WordCloud')
+    plt.show()
+    
 elif choice == 'time':
     from pyspark.sql.functions import hour, minute, col
     df_with_time = df.withColumn("hour", hour(col("timestamp"))).withColumn("minute", minute(col("timestamp")))
